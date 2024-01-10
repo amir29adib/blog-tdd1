c1f01534a6fdb8c6f25471a919770277dace228a
amir29adib <88646926+amir29adib@users.noreply.github.com>
2024-01-10 20:47:18 +0330

    home view test

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -2,9 +2,16 @@
 
 @section('content')
     <h1>{{ $post->title }}</h1>
+
     <ul>
         @foreach($comments as $comment)
             <li>{{ $comment->text  }}</li>
         @endforeach
     </ul>
+
+    @auth()
+        <form action="{{ route('single.comment', $post->id) }}" method="post">
+            <textarea name="text"></textarea>
+        </form>
+    @endauth
 @endsection
--- tests/Feature/Views/LayoutViewTest.php
+++ tests/Feature/Views/LayoutViewTest.php
@@ -2,21 +2,39 @@
 
 namespace Tests\Feature\Views;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class LayoutViewTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function testLayoutViewRenderedWhenUserIsAdmin()
     {
-        $response = $this->get('/');
+        $user = User::factory()->state(['type' => 'admin',])->create();
 
-        $response->assertStatus(200);
+        $this->actingAs($user);
+
+        $view = $this->view('layouts.layout');
+
+        $view->assertSee('<a href="/admin/dashboard">admin panel</a>' , false);
+    }
+
+    public function testLayoutViewRenderedWhenUserIsNotAdmin()
+    {
+        $user = User::factory()->state(['type' => 'user',])->create();
+
+        $this->actingAs($user);
+
+        $view = $this->view('layouts.layout');
+
+        $view->assertDontSee('<a href="/admin/dashboard">admin panel</a>' , false);
     }
 }
