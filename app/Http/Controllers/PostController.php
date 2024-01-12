97094e06d49651d9cef3bb3197834575b5fe0b22
amir29adib <88646926+amir29adib@users.noreply.github.com>
2024-01-12 17:30:20 +0330

    tdd for update method of post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -59,11 +59,20 @@ class PostController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function update(Request $request, Post $post)
     {
-        //
+        // request data (title, description, image, tags)
+        $post->update([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'image' => $request->input('image'),
+        ]);
+
+        $post->tags()->sync($request->input('tags'));
+
+        return redirect(route('post.index'))->with('message', 'the post has been updated');
     }
 
     /**
--- tests/Feature/Controllers/Admin/PostControllerTest.php
+++ tests/Feature/Controllers/Admin/PostControllerTest.php
@@ -106,4 +106,38 @@ class PostControllerTest extends TestCase
         );
     }
 
+    public function testUpdateMethod()
+    {
+        $user = User::factory()->admin()->create();
+        $data = Post::factory()->state(['user_id' => $user->id])->make()->toArray();
+        $post = Post::factory()
+            ->state(['user_id' => $user->id])
+            ->hasTags(rand(1, 5))
+            ->create();
+        $tags = Tag::factory()->count(rand(1,5))->create();
+
+        $this
+            ->actingAs($user)
+            ->patch(
+                route('post.update', $post->id),
+                array_merge(
+                    ['tags' => $tags->pluck('id')->toArray()],
+                    $data
+                )
+            )
+            ->assertSessionHas('message', 'the post has been updated')
+            ->assertRedirect(route('post.index'));
+
+        $this->assertDatabaseHas('posts', array_merge(['id' => $post->id] , $data));
+        $this->assertEquals(
+            $tags->pluck('id')->toArray(),
+            Post::where($data)->first()->tags()->pluck('id')->toArray()
+        );
+
+        $this->assertEquals(
+            $this->middlewares,
+            request()->route()->middleware()
+        );
+    }
+
 }
